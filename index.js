c043e211fe9adc012ce80ff0ed227caa79a49071
Melanie Penn <melanie.penn17@gmail.com>
2022-10-26 03:19:29 -0500

    commit tree 31cc4cbd44af8b49382fb014879bcbfa558d4b37

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        document.body.innerHTML = object["id"]
+        console.log(object);
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+        console.log(error.message);
+    })
+}
